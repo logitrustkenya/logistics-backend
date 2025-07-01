4efaa69c4ac8b5873555d1e9207ea6159965043f
Eugenekarewa <eugenekarewa223@gmail.com>
2025-07-02 01:04:37 +0300

    added the env config

--- lib/utils/constants.ts
+++ lib/utils/constants.ts
@@ -3,7 +3,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export const MONGODB_URI = process.env.MONGODB_URI || '';
-export const APP_URL = process.env.APP_URL || 'http://localhost:3000';
-export const NODE_ENV = process.env.NODE_ENV || 'development';
+export const APP_URL = process.env.APP_URL || 'https://logistics-enhanced-app.vercel.app/';
+export const NODE_ENV = process.env.NODE_ENV || 'production';
 export const REQUIRE_API_KEY = process.env.REQUIRE_API_KEY || 'false';
 export const API_KEY = process.env.API_KEY || '';
